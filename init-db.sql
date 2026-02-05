954067bc97d084bcbe577045e1eb98a08da67b8b
Skander Rekik <skander.rekik@sesame.com.tn>
2026-02-05 12:37:30 +0100

    Update database credentials for hospital service in docker-compose and init-db scripts

--- init-db.sql
+++ init-db.sql
@@ -6,3 +6,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Grant privileges
 GRANT ALL PRIVILEGES ON DATABASE medloop_db TO medloop;
+
+-- Create hospital database for Hospital Service
+CREATE DATABASE hospital;
+
+-- Grant privileges on hospital database
+GRANT ALL PRIVILEGES ON DATABASE hospital TO medloop;
+
+-- If you want to use the postgres user as specified in your docker-compose
+-- You can also grant to the default postgres superuser
+-- (Note: postgres superuser already has all privileges by default)
